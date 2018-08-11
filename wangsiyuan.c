a0163b73d569e6e1db1981d31d6c24812418a6cf
Wang Siyuan <wangsiyuan@Wangs-MacBook-Pro.local>
2018-08-11 23:27:37 +0800

    update

---
+++ wangsiyuan.c
@@ -0,0 +1,111 @@
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "math.h"
+//
+int global_count;
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    printf("%s__%d\n", "resize", global_count);
+    global_count++;
+    glViewport(0, 0, width, height);
+}
+
+//
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+    if (glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS)
+        printf("you input 9\n");
+}
+//
+int timeold_ms;
+int timenew_ms;
+int count = -1;
+
+int getfps()
+{
+    count++;
+    struct timeval start;
+    gettimeofday(&start, NULL);
+    int ms = start.tv_sec * 1000 + start.tv_usec/1000;;
+    if (count % 3 == 0)
+    {
+        timeold_ms = ms;
+        return -1;
+    }
+    else if (count % 3 == 1)
+    {
+        timenew_ms = ms;
+        return 1000/(timenew_ms - timeold_ms);
+    }
+    return -1;
+}
+//
+time_t ts;
+void timestamp()
+{
+    time(&ts);
+    printf("timeis:%ld\n", ts);
+}
+
+//
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f};
+//
+int main(void)
+{
+    GLFWwindow *window;
+    int *time;
+    int *time1;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(320, 240, 50, 50);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    float r = 0.01;
+    float step = 0.01;
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        // sleep
+        usleep(28000);
+        // get frame per second
+        int fps = getfps();
+        if (fps > 0) {
+            printf("fps:%d\n", fps);
+        }
+        // process input
+        processInput(window);
+
+        /* Render here */
+        r = plus_unit(r, step);
+
+        glClearColor(r, r, r, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
