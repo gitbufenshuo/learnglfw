8e1f03179080425b169f4e631ed76a39b18d151d
王思源 <sywang@firewinggames.com>
2018-08-13 21:06:09 +0800

    VAO and VBO and EBO

--- hello/hello_triangle.c
+++ hello/hello_triangle.c
@@ -8,9 +8,15 @@ float vertices[] = {
     0.5f, -0.5f, 0.0f, 1.0f,
     0.0f, 0.5f, 0.0f, 1.0f,
     0.0f, 0.0f, 0.0f, 0.8f,
-    0.0f, 0.0f, 0.0f, 0.8f,
-    0.0f, 0.9f, 0.0f, 0.8f,
-    0.9f, 0.0f, 0.0f, 0.8f};
+    -0.5f, -0.5f, 0.0f, 0.1f,
+    0.5f, -0.5f, 0.0f, 0.1f,
+    0.0f, 0.5f, 0.0f, 0.1f,
+    0.0f, 0.0f, 0.0f, 0.1f};
+unsigned int indices[] = {
+    // note that we start from 0!
+    0, 1, 2, // first triangle
+    1, 2, 3  // second triangle
+};
 unsigned int shaderProgram;
 void compile()
 {
@@ -35,6 +41,7 @@ void compile()
 }
 unsigned int VBO[] = {0, 0};
 unsigned int VAO[] = {0, 0};
+unsigned int EBO[] = {0, 0};
 int prepared = 0;
 void prepare_draw_triangle()
 {
@@ -49,8 +56,7 @@ void prepare_draw_triangle()
     // GEN VBO VAO
     glGenBuffers(2, VBO);
     glGenVertexArrays(2, VAO);
-
-
+    glGenBuffers(2, EBO);
 
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
@@ -58,30 +64,35 @@ void prepare_draw_triangle()
     // 3.0 bind VAO should be here before set vertex attribute
     glBindVertexArray(VAO[0]);
     // 3.1 then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(  0 ) );
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(0));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(  12 ) );
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(12));
     glEnableVertexAttribArray(1);
-
+    // 3.2 the EBO set
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12, indices, GL_STATIC_DRAW);
 
     // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, 12 * 4, vertices+16, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    // glBufferData(GL_ARRAY_BUFFER, 12 * 4, vertices + 16, GL_STATIC_DRAW);
     // 3.0 bind VAO should be here before set vertex attribute
     glBindVertexArray(VAO[1]);
     // 3.1 then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(  0 ) );
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(0));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(  12 ) );
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(12));
     glEnableVertexAttribArray(1);
-    // last. config is set, you can do render in the main loop
+    // 3.3 the EBO set
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12, indices + 3, GL_STATIC_DRAW);
 
+    // last. config is set, you can do render in the main loop
 }
 int jiou = 0;
 void draw_triangle()
 {
     jiou++;
     prepare_draw_triangle();
-    glBindVertexArray(VAO[jiou%2]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO[jiou % 2]);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 }
