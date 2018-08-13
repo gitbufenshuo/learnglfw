8d21a1800c3562e89ea1b43344d8c8be1fd9e2fa
王思源 <sywang@firewinggames.com>
2018-08-13 14:06:24 +0800

    go

--- newone.c
+++ newone.c
@@ -1,12 +1,15 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
 
-#include "math.h"
+#include "utils/math.h"
+#include "utils/time.h"
+#include "hello/hello.h"
 //
 int global_count;
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     printf("%s__%d\n", "resize", global_count);
     global_count++;
@@ -16,56 +19,69 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 //
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1);
-    if(glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS)
         printf("you input 9\n");
 }
 //
-
-//
 time_t ts;
-void timestamp(){
+void timestamp()
+{
     time(&ts);
     printf("timeis:%ld\n", ts);
 }
 
 int main(void)
 {
-    GLFWwindow* window;
-
+    GLFWwindow *window;
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
+        printf("glfwCreateWindow_error\n");
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+    //
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        printf("Failed to initialize OpenGL context\n");
+        return -1;
+    }
+    
     glViewport(320, 240, 50, 50);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    float r = 0.01;
-    float step = 0.01;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         // sleep
-        usleep(20000);
-        // timestamp
-        timestamp();
+        usleep(28000);
+        // get frame per second
+        int fps = getfps();
+        if (fps > 0)
+        {
+            printf("fps:%d\n", fps);
+        }
         // process input
         processInput(window);
 
         /* Render here */
-        r = plus_unit(r, step);
-        printf("%f\n", r);
-        glClearColor(r, r, r, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        change_smooth(0.01f);
+        draw_triangle();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -77,4 +93,3 @@ int main(void)
     glfwTerminate();
     return 0;
 }
-
