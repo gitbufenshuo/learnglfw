e009161581c7c24ac092f29b24fb58c64d35545f
Wang Siyuan <wangsiyuan@Wangs-MacBook-Pro.local>
2018-08-15 01:07:36 +0800

    for mat

--- hello/hello_triangle.c
+++ hello/hello_triangle.c
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <math.h>
 #include <stdlib.h>
-#include  <stdio.h>
+#include <stdio.h>
 #include "../utils/file.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "../utils/stb_image.h"
@@ -16,20 +16,19 @@ float vertices[] = {
     0.0f, 0.5f, 0.0f, 0.1f,
     0.0f, 0.0f, 0.0f, 0.1f,
     //texture
-    1.0f, 1.0f , 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f
-    };
+    1.0f, 1.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f};
 unsigned int indices[] = {
     // note that we start from 0!
     0, 1, 2, // first triangle
     1, 2, 3  // second triangle
 };
 float texCoords[] = {
-    0.0f, 0.0f,  // lower-left corner  
-    1.0f, 0.0f,  // lower-right corner
-    0.5f, 1.0f   // top-center corner
+    0.0f, 0.0f, // lower-left corner
+    1.0f, 0.0f, // lower-right corner
+    0.5f, 1.0f  // top-center corner
 };
-float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+float borderColor[] = {1.0f, 1.0f, 0.0f, 1.0f};
 unsigned int shaderProgram;
 void compile()
 {
@@ -42,26 +41,26 @@ void compile()
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertex_shader, NULL);
     glCompileShader(vertexShader);
-    int  success;
+    int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
+    if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("vvv--%s\n",infoLog);
+        printf("vvv--%s\n", infoLog);
     }
     // fragment
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
     glCompileShader(fragmentShader);
-    int  success1;
+    int success1;
     char infoLog1[512];
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success1);
-    if(!success1)
+    if (!success1)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog1);
-        printf("fff--%s\n",infoLog1);
+        printf("fff--%s\n", infoLog1);
     }
     //
     shaderProgram = glCreateProgram();
@@ -95,7 +94,7 @@ void prepare_draw_triangle()
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, 16 * 4, vertices, GL_STATIC_DRAW);
-   
+
     // 3.0 bind VAO should be here before set vertex attribute
     glBindVertexArray(VAO[0]);
     // 3.1 then set our vertex attributes pointers
@@ -104,8 +103,8 @@ void prepare_draw_triangle()
     glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(12));
     glEnableVertexAttribArray(1);
     //text
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)(32 * sizeof(float)));
-    glEnableVertexAttribArray(2);  
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)(32 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     // 3.2 the EBO set
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12, indices, GL_STATIC_DRAW);
@@ -120,10 +119,11 @@ void prepare_draw_triangle()
     unsigned char *data = stbi_load("./learng.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else {
+    else
+    {
         printf("fail to create texture");
     }
     stbi_image_free(data);
@@ -140,7 +140,7 @@ void prepare_draw_triangle()
     glEnableVertexAttribArray(1);
     // 3.3 the EBO set
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12, indices + 3, GL_STATIC_DRAW); 
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12, indices + 3, GL_STATIC_DRAW);
 
     // last. config is set, you can do render in the main loop
 }
