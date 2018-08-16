dec5d9db42ebc6c620a28b9f6bcd3b83212a2cc3
Wang Siyuan <wangsiyuan@Wangs-MacBook-Pro.local>
2018-08-17 00:05:57 +0800

    green value --> change

--- hello/hello_triangle.c
+++ hello/hello_triangle.c
@@ -172,8 +172,8 @@ void draw_triangle()
     jiou++;
     prepare_draw_triangle();
     float timeValue = glfwGetTime();
-    float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+    float change = (sin(timeValue) / 2.0f) + 0.5f;
     myShader->use((void *)myShader);
-    myShader->setFloat((void *)myShader, "change", greenValue);
+    myShader->setFloat((void *)myShader, "change", change);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
