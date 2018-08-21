df4c2101b6adc4f2ad45bde19283f19a55d81bcb
王思源 <sywang@firewinggames.com>
2018-08-21 20:16:46 +0800

    some 注释 for mat.c

--- utils/mat.c
+++ utils/mat.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+// 释放一个矩阵的内存
 void Mat4Free(ST_MAT4 *mat4)
 {
     if (mat4 == 0)
@@ -15,14 +16,17 @@ void Mat4Free(ST_MAT4 *mat4)
     }
     free(mat4);
 }
+// 返回矩阵 row行，column列 的元素值
 float Mat4Row_Column(ST_MAT4 *mat4, int row, int column)
 {
     return (mat4->element)[(column - 1) * 4 + (row - 1)];
 }
+// 设置矩阵 row行，column列的元素值
 void Mat4SetValue(ST_MAT4 *mat4, int row, int column, float value)
 {
     (mat4->element)[(column - 1) * 4 + (row - 1)] = value;
 }
+// 废弃不用
 ST_VEC4 *MatVec4(ST_MAT4 *mat4, ST_VEC4 *vec4)
 {
     float *mat4ele = mat4->element;
@@ -45,6 +49,7 @@ ST_VEC4 *MatVec4(ST_MAT4 *mat4, ST_VEC4 *vec4)
     }
     return vec4;
 }
+// 新建一个矩阵，kind == 0 意味着：单位矩阵
 ST_MAT4 *NewMat4(int kind)
 {
     ST_MAT4 *mat4 = malloc(sizeof(ST_MAT4));
@@ -60,6 +65,7 @@ ST_MAT4 *NewMat4(int kind)
     }
     return mat4;
 }
+// 废弃不用
 ST_VEC4 *NewVec4(float one, float two, float three, float four)
 {
     ST_VEC4 *vec4 = malloc(sizeof(ST_VEC4));
@@ -72,6 +78,7 @@ ST_VEC4 *NewVec4(float one, float two, float three, float four)
     return vec4;
 }
 
+// 打印一个矩阵
 void PrintMat4(ST_MAT4 *mat4)
 {
     printf("-\n");
@@ -81,6 +88,8 @@ void PrintMat4(ST_MAT4 *mat4)
         printf("%f %f %f %f\n", Mat4Row_Column(mat4, row, 1), Mat4Row_Column(mat4, row, 2), Mat4Row_Column(mat4, row, 3), Mat4Row_Column(mat4, row, 4));
     }
 }
+
+// 矩阵乘法
 ST_MAT4 *MatMat4(ST_MAT4 *mat4_left, ST_MAT4 *mat4_right)
 {
     ST_MAT4 *res = NewMat4(0);
@@ -102,6 +111,8 @@ ST_MAT4 *MatMat4(ST_MAT4 *mat4_left, ST_MAT4 *mat4_right)
     }
     return res;
 }
+
+// 一个矩阵变成单位矩阵
 void SetMat4Identity(ST_MAT4 *mat4)
 {
     memset(mat4->element, 0, sizeof(float) * 16);
@@ -110,10 +121,12 @@ void SetMat4Identity(ST_MAT4 *mat4)
     mat4->element[10] = 1.0f;
     mat4->element[15] = 1.0f;
 }
+// 角度变弧度
 float RadiusOfDegree(float degree)
 {
     return (3.141592653 * degree) / 180.0f;
 }
+// 齐次空间的旋转，先 Z 后 Y 再 X
 // x y z means z y x
 ST_MAT4 *D3_Rotate(ST_MAT4 *mat4, float x_degree, float y_degree, float z_degree)
 {
@@ -140,6 +153,7 @@ ST_MAT4 *D3_Rotate(ST_MAT4 *mat4, float x_degree, float y_degree, float z_degree
     return shouldReturn;
 }
 
+// 齐次空间的位移
 // x y z means z y x
 ST_MAT4 *D3_Translate(ST_MAT4 *mat4, float x_value, float y_value, float z_value)
 {
@@ -153,6 +167,7 @@ ST_MAT4 *D3_Translate(ST_MAT4 *mat4, float x_value, float y_value, float z_value
     Mat4Free(res);
     return shouldReturn;
 }
+// Z 坐标压缩，实现透视
 ST_MAT4 *D3_Homoz(ST_MAT4 *mat4)
 {
     ST_MAT4 *res = NewMat4(0);
@@ -162,6 +177,7 @@ ST_MAT4 *D3_Homoz(ST_MAT4 *mat4)
     Mat4Free(res);
     return shouldReturn;
 }
+// 齐次空间的拉伸
 ST_MAT4 *D3_Scale(ST_MAT4 *mat4, float x, float y, float z)
 {
     ST_MAT4 *res = NewMat4(0);
